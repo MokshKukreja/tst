8ff040a070152d07cc56006454e2d22ee9daf38c
Moksh Kukreja <98658751+MokshKukreja@users.noreply.github.com>
2024-05-23 14:51:05 +0530

    yuu

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const mongoose = require("mongoose")
+const cors = require("cors")
+const transactionRouter = require('./routes/transactionRoutes')
+const userRouter = require('./routes/userRoutes')
+require('dotenv').config();
 
 
 app.use(express.json())
 
 
-// app.use(cors())
+app.use(cors())
 
 
-// app.use((req,res,next)=>{
-//     console.log("HTTPS Method" + req.method + " , URL" +req.url)
-//     next();
-// })
-mongoose.connect("mongodb+srv://mokshkukreja999:moksh90@cluster0.frzozja.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0", () => {
-    console.log("Connected successfully");
+app.use((req,res,next)=>{
+    console.log("HTTPS Method" + req.method + " , URL" +req.url)
+    next();
 })
-mongoose.set('strictQuery', true);
+app.use("/transactions",transactionRouter)
+app.use("/users",userRouter)
+
+// mongoose.connect("mongodb+srv://mokshkukreja999:moksh90@cluster0.frzozja.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0", () => {
+//     console.log("Connected successfully");
+// })
+// mongoose.set('strictQuery', true);
 
 
 
