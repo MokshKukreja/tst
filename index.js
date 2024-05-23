332142b6f2e9161a6a159e759d2303ab0162ffe5
Moksh Kukreja <98658751+MokshKukreja@users.noreply.github.com>
2024-05-23 14:56:56 +0530

    k

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(cors())
 // app.use("/transactions",transactionRouter)
 // app.use("/users",userRouter)
 
-// mongoose.connect("mongodb+srv://mokshkukreja999:moksh90@cluster0.frzozja.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0", () => {
-//     console.log("Connected successfully");
-// })
-// mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://mokshkukreja999:moksh90@cluster0.frzozja.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0", () => {
+    console.log("Connected successfully");
+})
+mongoose.set('strictQuery', true);
 
 
 
