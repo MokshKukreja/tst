623a9de240abb3b1e19037afcd4b276efdb4acc1
Moksh Kukreja <98658751+MokshKukreja@users.noreply.github.com>
2024-05-23 14:48:40 +0530

    comment

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const app = express()
 const port = 8000
 
 
+app.use(express.json())
+
+
+// app.use(cors())
+
+
+// app.use((req,res,next)=>{
+//     console.log("HTTPS Method" + req.method + " , URL" +req.url)
+//     next();
+// })
+mongoose.connect("mongodb+srv://mokshkukreja999:moksh90@cluster0.frzozja.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0", () => {
+    console.log("Connected successfully");
+})
+mongoose.set('strictQuery', true);
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
